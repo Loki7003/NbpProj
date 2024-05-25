8b4ac3b2399a1a2bd9b810731568ae28cce45e6b
Loki7003 <80339875+Loki7003@users.noreply.github.com>
2024-05-25 17:48:26 +0200

    Update views.py

--- NBP_Django_API/NBP_Django_API/views.py
+++ NBP_Django_API/NBP_Django_API/views.py
@@ -1,28 +1,27 @@
 from django.http import JsonResponse
 from .models import Currency
 import requests
-from django.db.models import Count
-from django.db.models.functions import ExtractYear, ExtractQuarter, ExtractMonth
 from datetime import datetime
 
 def update_rates(request):
-    response = requests.get('http://api.nbp.pl/api/exchangerates/tables/A/')
+    end_date = datetime.today().date()
+    start_date = end_date - timedelta(days=93)
+
+    url = f"http://api.nbp.pl/api/exchangerates/tables/A/{start_date}/{end_date}/"
+    response = requests.get(url)
     data = response.json()
+
     rates_data = []
-    current_date = datetime.strptime(data[0]['effectiveDate'], '%Y-%m-%d').date()
     for item in data[0]['rates']:
         rates_data.append({
-            'code' : item['code'],
-            'rate' : item['mid'],
-            'date' : data[0]['effectiveDate']
+            'code': item['code'],
+            'rate': item['mid'],
+            'date': data[0]['effectiveDate']
         })
-        last_saved_currency = Currency.objects.filter(code=item['code']).order_by('-date').first()
-        if not last_saved_currency or last_saved_currency.date != current_date:
+
+        last_saved_currency = Currency.objects.filter(code=item['code'], date=current_date).first()
+        if not last_saved_currency:
             currency = Currency(code=item['code'], rate=item['mid'], date=current_date)
             currency.save()
-    return JsonResponse({'status': 'success', 'data' : rates_data})
 
-
-def get_currency_details(request):
-    data = Currency.objects.values('code', 'rate', 'date')
-    return JsonResponse(list(data), safe=False)
+    return JsonResponse({'status': 'success', 'data': rates_data})
